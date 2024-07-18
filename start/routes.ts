518d85e1895818624533f8b333d11d281a351593
AlexisGadbin <alexis@gadbin.com>
2024-07-18 21:31:35 +0200

    fix: api prefix

--- start/routes.ts
+++ start/routes.ts
@@ -13,41 +13,45 @@ import { middleware } from './kernel.js'
 const AuthorsController = () => import('#controllers/authors_controller')
 const BooksController = () => import('#controllers/books_controller')
 
-router.get('/', async () => {
-  return {
-    hello: 'world',
-  }
-})
-
 router
   .group(() => {
-    router.delete('logout', [AuthController, 'logout'])
-    router.get('current-user', [AuthController, 'currentUser'])
-  })
-  .prefix('auth')
-  .use(middleware.auth())
+    router.get('/', async () => {
+      return {
+        hello: 'world',
+      }
+    })
 
-router
-  .group(() => {
-    router.post('login', [AuthController, 'login'])
-    router.post('register', [AuthController, 'register'])
-  })
-  .prefix('auth')
-  .use(middleware.guest())
+    router
+      .group(() => {
+        router.delete('logout', [AuthController, 'logout'])
+        router.get('current-user', [AuthController, 'currentUser'])
+      })
+      .prefix('auth')
+      .use(middleware.auth())
 
-router
-  .group(() => {
-    router.get('', [BooksController, 'getBooks'])
-    router.get(':id', [BooksController, 'getBook'])
-    router.post('', [BooksController, 'createBook'])
-    router.delete(':id', [BooksController, 'deleteBook'])
-  })
-  .prefix('books')
-  .use(middleware.auth())
+    router
+      .group(() => {
+        router.post('login', [AuthController, 'login'])
+        router.post('register', [AuthController, 'register'])
+      })
+      .prefix('auth')
+      .use(middleware.guest())
 
-router
-  .group(() => {
-    router.get('', [AuthorsController, 'getAuthors'])
+    router
+      .group(() => {
+        router.get('', [BooksController, 'getBooks'])
+        router.get(':id', [BooksController, 'getBook'])
+        router.post('', [BooksController, 'createBook'])
+        router.delete(':id', [BooksController, 'deleteBook'])
+      })
+      .prefix('books')
+      .use(middleware.auth())
+
+    router
+      .group(() => {
+        router.get('', [AuthorsController, 'getAuthors'])
+      })
+      .prefix('authors')
+      .use(middleware.auth())
   })
-  .prefix('authors')
-  .use(middleware.auth())
+  .prefix('api')
