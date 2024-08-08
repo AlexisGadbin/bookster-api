4120328239709a02067a29391f607e33821fa712
AlexisGadbin <alexis@gadbin.com>
2024-08-08 22:57:36 +0200

    fix: mapper for book isWishlisted

--- app/mappers/book_mapper.ts
+++ app/mappers/book_mapper.ts
@@ -11,6 +11,7 @@ export default class BookMapper {
       contributor: book.contributor,
       coverImageUrl: book.coverImageUrl,
       backCoverImageUrl: book.backCoverImageUrl,
+      isWishlisted: book.isWishlisted,
     }
   }
 }
